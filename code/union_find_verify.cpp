14a83bfda2f17553d63a2124795ef367f02fca19
Shuya Nakagawa <BanetteGin@gmail.com>
2023-08-08 00:26:45 +0900

    Update union_find_verify.cpp

--- code/union_find_verify.cpp
+++ code/union_find_verify.cpp
@@ -24,8 +24,35 @@ typedef vector<vector<bool> > vvbol;
 typedef vector<ll> vll;
 typedef vector<vector<ll> > vvll;
 
-#include "../src/union_find.hpp"
-using namespace BanetteGin;
+template <class T>
+struct union_find {
+    vector<T> par, rk, sz;
+    union_find(T n)
+        : par(n, -1), rk(n, 0), sz(n, 1) {
+    }
+    T root(T& v) {
+        if (par[v] == -1)
+            return v;
+        else
+            return par[v] = root(par[v]);
+    }
+    bool same(T& u, T& v) {
+        return root(u) == root(v);
+    }
+    bool unite(T& u, T& v) {
+        T urt = root(u);
+        T vrt = root(v);
+        if (urt == vrt) return false;
+        if (rk[urt] < rk[vrt]) swap(urt, vrt);
+        par[vrt] = urt;
+        if (rk[urt] == rk[vrt]) rk[urt]++;
+        sz[urt] += sz[vrt];
+        return true;
+    }
+    T size(T& v) {
+        return sz[root(v)];
+    }
+};
 
 int main(void) {
     ll n, q;
