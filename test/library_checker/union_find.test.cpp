593edc0fa5064cdec8db1999f5f12333823876e7
Shuya Nakagawa <BanetteGin@gmail.com>
2023-08-08 11:26:59 +0900

    fix

---
+++ test/library_checker/union_find.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (ll i = 0, n_f = (ll)(n); i < n_f; ++i)
+
+typedef long long ll;
+
+#include "src/union_find.hpp"
+
+int main(void) {
+    ll n, q;
+    cin >> n >> q;
+    union_find uf(n);
+    rep(i, q) {
+        ll t, u, v;
+        cin >> t >> u >> v;
+        if (t == 0)
+            uf.unite(u, v);
+        else {
+            if (uf.same(u, v))
+                cout << 1 << endl;
+            else
+                cout << 0 << endl;
+        }
+    }
+}
