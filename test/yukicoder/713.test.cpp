c67d6ef7c31b8bc386e8fad1b07285fff750c78e
Shuya Nakagawa <BanetteGin@gmail.com>
2023-09-08 16:50:21 +0900

    Create 713.test.cpp

---
+++ test/yukicoder/713.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://yukicoder.me/problems/no/713"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (ll i = 0, n_f = (ll)(n); i < n_f; ++i)
+
+typedef long long ll;
+
+#include "../../src/number_theory/naive_sieve.hpp"
+
+int main(void) {
+    ll n;
+    cin >> n;
+    auto a = BanetteGin::naive_sieve(n);
+    ll ans = 0;
+    rep(i, a.size()) ans += a[i];
+    cout << ans << endl;
+    return 0;
+}
