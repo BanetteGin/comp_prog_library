587e9754541aa588ab943089909e05c8711f58a6
Shuya Nakagawa <BanetteGin@gmail.com>
2023-09-08 16:51:51 +0900

    Update naive_sieve.hpp

--- src/number_theory/naive_sieve.hpp
+++ src/number_theory/naive_sieve.hpp
@@ -10,7 +10,9 @@ namespace BanetteGin {
 template <class T>
 std::vector<T> naive_sieve(T n) {
     std::vector<T> prime_list;
-    for (long long int i = 2; i <= n; ++i) {
+    if (n < 2) return prime_list;
+    prime_list.emplace_back(2);
+    for (long long int i = 3; i <= n; i += 2) {
         if (prime_check(i)) prime_list.emplace_back(i);
     }
     return prime_list;
